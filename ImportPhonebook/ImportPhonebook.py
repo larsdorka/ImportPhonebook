7423fa040e653910030c9b95d0e4399ce212ccc1
larsdorka <lars.dorka@googlemail.com>
2020-01-25 17:31:11 +0100

    Finish phone book creation

--- ImportPhonebook/ImportPhonebook.py
+++ ImportPhonebook/ImportPhonebook.py
@@ -1,5 +1,23 @@
 import argparse
 import csv
+import xml.etree.ElementTree as ET
+
+
+def create_contact():
+    contact = ET.Element("Contact")
+    ET.SubElement(contact, "FirstName").text = ""
+    ET.SubElement(contact, "IsPrimary").text = "false"
+    ET.SubElement(contact, "Primary").text = "0"
+    ET.SubElement(contact, "Frequent").text = "0"
+    ET.SubElement(contact, "Ringtone").text = "content://settings/system/ringtone"
+    ET.SubElement(contact, "PhotoUrl")
+    organization = ET.SubElement(contact, "organization")
+    ET.SubElement(organization, "department")
+    ET.SubElement(organization, "rank")
+    ET.SubElement(organization, "company")
+    ET.SubElement(organization, "title")
+    ET.SubElement(contact, "address")
+    return contact
 
 
 # main application
@@ -21,18 +39,31 @@ if __name__ == '__main__':
     if args.o is not None:
         output_filename = args.o
 
+    addressbook = ET.Element("AddressBook")
+    ET.SubElement(addressbook, "version").text = "1"
+
     if internal_filename != "":
-        print()
-        print('Internal numbers:')
         with open(internal_filename, newline='') as i_file:
             i_reader = csv.DictReader(i_file, delimiter=';')
             for row in i_reader:
-                print(row['Name'].strip(), row['Number'].strip())
+                newcontact = create_contact()
+                ET.SubElement(newcontact, "LastName").text = row['Name'].strip()
+                phone = ET.SubElement(newcontact, "Phone", type="Work")
+                ET.SubElement(phone, "phonenumber").text = row['Number'].strip()
+                ET.SubElement(phone, "accountindex").text = "-1"
+                addressbook.append(newcontact)
 
     if external_filename != "":
-        print()
-        print('External numbers:')
         with open(external_filename, newline='') as e_file:
             e_reader = csv.DictReader(e_file, delimiter=";")
             for row in e_reader:
-                print(row['Name'].strip(), row['Number'].strip())
+                newcontact = create_contact()
+                ET.SubElement(newcontact, "LastName").text = row['Name'].strip()
+                phone = ET.SubElement(newcontact, "Phone", type="Mobile")
+                ET.SubElement(phone, "phonenumber").text = row['Number'].strip()
+                ET.SubElement(phone, "accountindex").text = "-1"
+                addressbook.append(newcontact)
+
+    # print(ET.tostring(addressbook, encoding="UTF-8", method="xml"))
+    xml = ET.ElementTree(element=addressbook)
+    xml.write(output_filename, encoding="UTF-8", xml_declaration=True, method="xml")
