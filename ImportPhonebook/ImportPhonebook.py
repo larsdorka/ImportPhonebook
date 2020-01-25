5780cdc282b3e69601c287b475e1f5b40b27edca
larsdorka <lars.dorka@googlemail.com>
2020-01-25 15:08:18 +0100

    Add initial argument parser

--- ImportPhonebook/ImportPhonebook.py
+++ ImportPhonebook/ImportPhonebook.py
@@ -1,3 +1,10 @@
+import argparse
+
+
 # main application
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description='Convert CVS netphone phonebooks into XML import file')
+    parser.add_argument('-i', help='csv file of internal entries')
+    parser.add_argument('-e', help='csv file of external entries')
+    parser.add_argument('-o', help='file to store the xml output in')
+    parser.print_help()
